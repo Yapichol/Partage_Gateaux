fd79cc5205e83958c49f72fe4f3470c1b90c7da6
Yapichol <98472802+Yapichol@users.noreply.github.com>
2022-03-07 20:16:40 +0100

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,11 +1,14 @@
 #main
 
 from Graphe import *
+from MainWindow import *
 
 if __name__=="__main__":
 
     print("Debut")
-    aleatoire = False
+    aleatoire = True
+
+    graph = None
     
     #on test a vec un graphe que l'on crée
     if not aleatoire:
@@ -19,29 +22,29 @@ if __name__=="__main__":
         g.ajouter_arc("B", "A")
         g.ajouter_liste_arcs([("A","C"),("A","D"),("B","E")])
 
-        #g.afficher_graphe()
-        g.affiche()
+        g.afficher_graphe()
         g.partage_aleatoire()
         g.afficher_graphe()
-        stable,pas_stable= g.est_stable()
-        print("Le graphe est stable? ",stable)
-        print("Noeuds pas stables:",pas_stable)
-        #print("\nDEVENIR STABLE")
-        #g.devenir_stable(pas_stable)
-
         
+        g.affiche()
+        graph = g
+
     #on test avec un graphe généré aléatoirement
     else :
         
         g2 = Graphe()
-        g2.generer_graphe(5,0.2)
+        g2.generer_graphe(6,0.2)
         g2.afficher_graphe()
         g2.affiche()
         g2.partage_aleatoire()
         g2.afficher_graphe()
-        stable,pas_stable= g2.est_stable()
-        print("Le graphe est stable? ",stable)
-        print("Noeuds pas stables:",pas_stable)
-        
-        
+        graph = g2
+
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+    window.import_graph(graph)
+    app.exec_()
+    
     print("Fin")
